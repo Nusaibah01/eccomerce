d03bed5fc5a8273287aecd069567099f02d733ca
Nusaibah01 <charifnusaibah01@gmail.com>
2024-06-11 14:50:50 +0200

    day2iternary

--- script/code.js
+++ script/code.js
@@ -1,48 +1,48 @@
 // Products
-let products= JSON.parse(localStorage.getItem('products')) ? JSON.parse
-(localStorage.getItem('products')) : localStorage.setItem(
-    'products',
+let books= JSON.parse(localStorage.getItem('books')) ? JSON.parse
+(localStorage.getItem('books')) : localStorage.setItem(
+    'books',
 JSON.stringify(
     [
 {
     id: 1,
     bookName:"Jane Eyre",
     genre: "Historical",
-    description: "Jane Eyre: Deluxe Hardbound Edition by Charlotte Bront: follows the story of Jane, a seemingly plain and simple girl as she battles through life's struggles.",
-    amount: 439.00 ,
-    img_url: "https://nusaibah01.github.io/e-images/images/Jane Eyre_Deluxe Hardbound Edition.jpg"
+    description: "Jane Eyre: Deluxe Hardbound Edition by Charlotte Bront:Follows the story of Jane, a seemingly plain and simple girl as she battles through life's struggles.",
+    price: 439.00 ,
+    img_url: "https://nusaibah01.github.io/all-images/images/Jane.jpg"
 },
 {
     id: 2,
     bookName:"Caraval",
     genre: "High Fantasy",
     description: "The sweeping tale of two sisters who escape their ruthless father when they enter the dangerous intrigue of a legendary game ",
-    amount: 443.00,
-    img_url: "https://nusaibah01.github.io/e-images/images/"
+    price: 443.00,
+    img_url: "https://nusaibah01.github.io/all-images/Caravel.jpg"
 },
 {
     id: 3,
     bookName:"As Long As the Lemon Trees Grow",
     genre: "Fiction",
     description: "Set in Syria during the country's civil war, 'As Long as the Lemon Trees Grow' follows 18-year-old Salama Kassab as she navigates life under a cruel dictatorship. It's a story of unwavering love for one's country and the resilience that sustains people even in the darkest of times",
-    amount: 350.00,
-    img_url: "https://nusaibah01.github.io/e-images/images/As Long As the Lemon Trees Grow.jpg"
+    price: 350.00,
+    img_url: "https://nusaibah01.github.io/all-images/Lemon.jpg"
 },
 {
     id: 4,
     bookName:"Circe",
     genre: "Mythology",
     description: "The chronicle of an outsider woman who uses her power and wits to protect herself and the people she loves, ultimately looking within to define herself.",
-    amount: 400.00,
-    img_url: "https://nusaibah01.github.io/e-images/images/Circe - Madeline Miller.jpg"
+    price: 400.00,
+    img_url: "https://nusaibah01.github.io/all-images/images/Circe.jpg"
 },
 {
     id: 5,
     bookName:"Prices for The Butterflys burden",
     genre: "Poetry",
     description: "A collection of poems which provide continual contrasts, balancing old literary traditions with new, highlighting lyrical, loving reflections alongside a bitter longing for the Palestine that was lost when Israel was created",
-    amount: 500.00,
-    img_url: "https://nusaibah01.github.io/e-images/images/Prices for The Butterfly's Burden by Mahmoud Darwish.jpg"
+    price: 500.00,
+    img_url: "https://nusaibah01.github.io/all-images/Burden.jpg"
 },
 
 ]
--- script/products.js
+++ script/products.js
@@ -1,25 +1,75 @@
-// 
+// Products
+let container = document.querySelector('[ourBooks]')
+let bookSearch = document.querySelector('[findBook]')
+let sortByPrice = document.querySelector('[sort]')
+let books = JSON.parse(
+    localStorage.getItem(books)
+)
+// Display all books in my store
+function displayBooks(args){
+container.innerHTML = ""
+try{
+    args.forEach(book => {
+container.innerHTML += `
+  <div class="card">
+  <img src="${book.img_url}" class="card-img-top" alt="${book.bookName}" loading='lazy'>
+  <div class="card-body">
+    <h5 class="card-title">${book.bookName}</h5>
+    <p class="card-text">${book.description}</p>
+    <p class="card-text">
+     <span class="shadow text-success fw-bold">Price</span>
+     R${book.price}
+     </p>
+    <button type='button' class="btn btn-success" onclick='addToCart(${JSON.stringify(book)})'><i class="bi bi-cart4"></i></button>
+  </div>
+</div>`
+})
+    
+}
+catch (e){
+container.textContent = "Please try again later"
+}
+}
+displayBooks(books)
 
+// find book
+bookSearch.addEventListener('keyup', () => {
+    try {
+        if (searchBook.value.length < 1) {
+            displayBooks(books)
+        }
+        let filteredBooks = books.filter(book => book.bookName.toLowerCase().includes(searchBook.value))
+        displayBooks(filteredBooks)
+        if (!filteredBook.length) throw new Error(`${searchBook.value} book was not found. To request it's addition to our collection<br>
+            <label>Click Here:</label>
+             <button><a href="../html/contact.html">Enquiries</button>`)
+    } catch (e) {
+        container.textContent = e.message || 'Please try again later'
+    }
+})
 
+// Sort by Price 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let isToggle = false
+sortingByAmount.addEventListener('click', () => {
+    try {
+        if (!books) throw new Error('Please try again later')
+        if (!isToggle) {
+            books.sort((a, b) => b.price - a.price)
+            sortingByAmount.textContent = 'Sorted by highest amount'
+            isToggle = true
+        } else {
+            books.sort((a, b) => a.price - b.price)
+            sortingByAmount.textContent = 'Sorted by lowest amount'
+            isToggle = false
+        }
+        displayBooks(books)
+    } catch (e) {
+        container.textContent = e.message || 'We are working on this issue'
+    }
+})
 
 
 
 // Display the current year
-document.querySelector('[currentYear]').textContent =new Date().getUTCFullYear()
+document.querySelector('[cY]').textContent =new Date().getUTCFullYear()
